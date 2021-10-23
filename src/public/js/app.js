abc1bc6c38d04108a9ba49d893972c0c2be6ede0
HyunHo <sush0506@naver.com>
2021-10-23 12:34:58 +0900

    Socket으로 브라우저와 서버가 서로 메세지 주고받기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,19 @@
 // 프론트엔드
 const socket = new WebSocket(`ws://${window.location.host}`);
-// 백엔드와 실시간으로 연결할 socket을 만듬. 프로토콜을 ws로 바꿔줘야 한다
-// 다른 기기에서도 볼 수 있도록 host 주소로 지정
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+})
+// 서버와 연결되었을 때 발생하는 이벤트
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+})
+// 서버로부터 메세지를 받았을 때 발생하는 이벤트
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server");
+})
+// 서버와의 연결이 끊어졌을 때 발생하는 이벤트
+setTimeout(() => {
+    socket.send("hello from the brouser!");
+    // 서버로 메세지를 보냄
+},10000)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 // 백엔드
-// express를 사용한 NodeJS 설정
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
@@ -9,24 +8,26 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + '/views');
 app.use("/public", express.static(__dirname + "/public"));
-// 유저가 볼 수 있는 폴더를 따로 지정
 app.get("/", (req,res) => res.render("home"));
 app.get("/*", (req,res) => res.redirect("/"));
-// '/'뒤에 다른것을 입력해도 home으로 이동
 
 const handleListen = () => console.log('Listening on http://localhost:3000')
 
 const server = http.createServer(app);
-// express app으로부터 서버 만들기
 const wss = new WebSocket.Server({server}); 
-// http 서버 위에 webSocket 서버를 만듬
-// 같은 서버에서 http와 ws가 같은 포트에서 작동
-// 뒤의 Server는 http를 사용하고 싶지 않으면 쓰지 않는다(필수가 아님)
-function handleConnection(socket) {
-    console.log(socket)
-}
-wss.on("connection", handleConnection);
-// webSocket Events
+
+wss.on("connection", (socket) => {
+    console.log("Connerted to browser");
+    // 브라우저와 연결되면 서버에 이벤트 발생
+    socket.on("close", () => console.log("Disconnected from the browser"));
+    // browser 탭을 닫으면 서버에 이벤트 발생
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    })
+    // 브라우저로부터 메세지를 받으면 이벤트가 발생
+    socket.send("hello!!!");
+    // socket의 메소드로 socket에 메세지를 보냄
+});
 
 server.listen(3000, handleListen);
 
