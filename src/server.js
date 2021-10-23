c7bb9a8eadff981664bfa002263ba2a55f8a9172
HyunHo <sush0506@naver.com>
2021-10-23 11:31:02 +0900

    WebSocket 설치 & http 서버와 함께 사용

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 // 백엔드
 // express를 사용한 NodeJS 설정
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -13,4 +15,12 @@ app.get("/*", (req,res) => res.redirect("/"));
 // '/'뒤에 다른것을 입력해도 home으로 이동
 
 const handleListen = () => console.log('Listening on http://localhost:3000')
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+// express app으로부터 서버 만들기
+const wss = new WebSocket.Server({server}); 
+// http 서버 위에 webSocket 서버를 만듬
+// 같은 서버에서 http와 ws가 같은 포트에서 작동
+// 뒤의 Server는 http를 사용하고 싶지 않으면 쓰지 않는다(필수가 아님)
+server.listen(3000, handleListen);
+
