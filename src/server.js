cb768093e3eb4b17dba9eb3147f0d9da1ae73fa9
HyunHo <sush0506@naver.com>
2021-10-24 22:22:22 +0900

    Nickname 완성 : socket 객체를 활용하여 닉네임을 저장하기

--- src/server.js
+++ src/server.js
@@ -20,10 +20,23 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon"
+    // 닉네임을 정하지 않은 익명의 이용자를 위해 socket 생성시 닉네임의 디폴트 값을 지정
     console.log("Connerted to browser");
     socket.on("close", () => console.log("Disconnected from the browser"));
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString('utf-8')));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        // string을 다시 JavaScirpt Object로 바꾼다
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                // "닉네임 : 메세지" 형식으로 전송
+            case "nickname":
+                socket["nickname"] = message.payload;
+                // socket은 기본적으로 객체
+                // socket의 "nickname" property를 새로운 닉네임으로 변경
+        // JSON에서 type을 확인하고 그에 해당하는 payload를 보내준다
+        }
     })
 });
 
