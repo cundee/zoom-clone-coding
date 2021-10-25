d065baecec73cf0b9176e49e1a7cbe7d4e9933f1
HyunHo <sush0506@naver.com>
2021-10-25 10:20:33 +0900

    nickname을 함께 입력받고 room에 입장하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,7 +2,6 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
 room.hidden = true;
@@ -28,11 +27,10 @@ function handleMessageSubmit(event) {
 
 function handleNicknameSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector("#name input");
+    const input = welcome.querySelector("#name input");
     const value = input.value;
     socket.emit("nickname", input.value);
-    input.value = "";
-    // input을 id로 구분하여 nickname을 입력받아 백엔드에 보낸다
+    
 }
 
 function showRoom() {
@@ -41,20 +39,24 @@ function showRoom() {
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
     const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
+    
     msgForm.addEventListener("submit", handleMessageSubmit);
-    nameForm.addEventListener("submit", handleNicknameSubmit);
+    
 }
 
 function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = form.querySelector("input");
+    const input = roomNameForm.querySelector("#roomname input");
     socket.emit("enter_room", input.value, showRoom);
     roomName = input.value;
     input.value = "";
 }
 
-form.addEventListener("submit", handleRoomSubmit);
+
+const nameForm = welcome.querySelector("#name");
+const roomNameForm = welcome.querySelector("#roomname");
+nameForm.addEventListener("submit", handleNicknameSubmit);
+roomNameForm.addEventListener("submit", handleRoomSubmit);
 
 socket.on("welcome", (user) => {
    addMessage(`${user} joined!`);
