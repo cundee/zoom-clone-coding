9f01392bf4a05331f39f573e4f525618a436819c
HyunHo <sush0506@naver.com>
2021-10-31 11:48:32 +0900

    welcome과 call을 분리하고, room을 생성하여 방에 참가

--- src/server.js
+++ src/server.js
@@ -6,16 +6,21 @@ import express from "express";
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + '/views');
+app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home"));
-app.get("/*", (req,res) => res.redirect("/"));
-
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-const handleListen = () => console.log('Listening on http://localhost:3000')
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+});
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
-
-
